06abcf34183d7f101a9f2ee856c2c42710592602
Anthony Cardona <keilorcardona0912@outlook.com>
2019-02-22 10:40:36 -0600

    correcciones en la llamada de la tabla en la base de datos

--- Practicas_Prof/Sessiones/shared/db.php
+++ Practicas_Prof/Sessiones/shared/db.php
@@ -4,5 +4,5 @@ require_once __DIR__ . '/../Db/PgConnection.php';
 
 use Db\PgConnection;
 
-$con = new PgConnection('postgres', 'postgres', 'users', 5432, 'localhost');
+$con = new PgConnection('postgres', 'postgres', 'university', 5432, 'localhost');
 $con->connect();
